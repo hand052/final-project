ba659054f0c3cabeae75e92f9c2c69dfef9829b3
n1tz <cole.masaki@gmail.com>
2020-04-07 14:42:51 +0900

    uploaded project

---
+++ Evo Sim/Assets/creatureScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creatureScript : MonoBehaviour
+{
+    public float distance;
+
+    public GameObject R_Arm;
+    public HingeJoint R_Arm_Joint;
+
+    public GameObject L_Arm;
+    public HingeJoint L_Arm_Joint;
+
+    public GameObject R_Leg;
+    public HingeJoint R_Leg_Joint;
+
+    public GameObject L_Leg;
+    public HingeJoint L_Leg_Joint;
+
+
+    //Right Arm Values
+    public float RA_f1;
+    public float RA_t1;
+    public float RA_f2;
+    public float RA_t2;
+    //Left Arm Values
+    public float LA_f1;
+    public float LA_t1;
+    public float LA_f2;
+    public float LA_t2;
+
+    //Right Leg Values
+    public float RL_f1;
+    public float RL_t1;
+    public float RL_f2;
+    public float RL_t2;
+    //Left Leg Values
+    public float LL_f1;
+    public float LL_t1;
+    public float LL_f2;
+    public float LL_t2;
+
+    void Start()
+    {
+       
+
+        //Get Hinge Joints
+        R_Arm_Joint = R_Arm.GetComponent<HingeJoint>();
+        L_Arm_Joint = L_Arm.GetComponent<HingeJoint>();
+        R_Leg_Joint = R_Leg.GetComponent<HingeJoint>();
+        L_Leg_Joint = L_Leg.GetComponent<HingeJoint>();
+        StartCoroutine("DelayedStart");
+    }
+
+    IEnumerator DelayedStart()
+    {
+        //Start moving Joints 1 second late so values can be set during instantiation (this is inefficient but a temporary bug fix)
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(Move(R_Arm_Joint, RA_f1, RA_t1, RA_f2, RA_t2));
+        StartCoroutine(Move(L_Arm_Joint, LA_f1, LA_t1, LA_f2, LA_t2));
+        StartCoroutine(Move(R_Leg_Joint, RL_f1, RL_t1, RL_f2, RL_t2));
+        StartCoroutine(Move(L_Leg_Joint, LL_f1, LL_t1, LL_f2, LL_t2));
+    }
+
+    
+    IEnumerator Move(HingeJoint joint, float force1, float time1, float force2, float time2)
+    {
+        JointMotor motor = joint.motor;
+        motor.force = 1000;
+        motor.targetVelocity = force1;
+        joint.motor = motor;
+        yield return new WaitForSeconds(time1);
+        motor.force = 1000;
+        motor.targetVelocity = -force2;
+        joint.motor = motor;
+        yield return new WaitForSeconds(time2);
+       
+        StartCoroutine(Move(joint,force1,time1,force2,time2));
+    }
+    
+    void Update()
+    {
+        //Distance from Spawn
+        distance = Vector3.Distance(transform.position, Vector3.zero);
+
+    }
+}
---
+++ Evo Sim/Assets/spawnerScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnerScript : MonoBehaviour
+{
+    public float waveTime = 10f;
+    public GameObject creature1;
+    public float mutatateRate = 10f;
+
+    public GameObject[] currentCreatures;
+
+    void Start()
+    {
+        FirstSpawn();
+    }
+
+    //Purely Randomized Spawn
+    void FirstSpawn()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject creature = (GameObject)Instantiate(creature1, transform.position, Quaternion.identity);
+
+            //Set layers of creature bodyparts
+            creature.transform.GetChild(0).gameObject.layer = i + 8;
+            creature.transform.GetChild(1).gameObject.layer = i + 8;
+            creature.transform.GetChild(2).gameObject.layer = i + 8;
+            creature.transform.GetChild(3).gameObject.layer = i + 8;
+            creature.transform.GetChild(4).gameObject.layer = i + 8;
+
+
+            creatureScript CS = creature.GetComponentInChildren<creatureScript>();
+            //randomly set locomotion values
+            CS.RA_f1 = Random.Range(10f, 500f);
+            CS.RA_t1 = Random.Range(0f, 2f);
+            CS.RA_f2 = Random.Range(10f, 500f);
+            CS.RA_t2 = Random.Range(0f, 2f);
+
+            CS.LA_f1 = Random.Range(10f, 500f);
+            CS.LA_t1 = Random.Range(0f, 2f);
+            CS.LA_f2 = Random.Range(10f, 500f);
+            CS.LA_t2 = Random.Range(0f, 2f);
+
+            CS.RL_f1 = Random.Range(10f, 500f);
+            CS.RL_t1 = Random.Range(0f, 2f);
+            CS.RL_f2 = Random.Range(10f, 500f);
+            CS.RL_t2 = Random.Range(0f, 2f);
+
+            CS.LL_f1 = Random.Range(10f, 500f);
+            CS.LL_t1 = Random.Range(0f, 2f);
+            CS.LL_f2 = Random.Range(10f, 500f);
+            CS.LL_t2 = Random.Range(0f, 2f);
+        }
+        StartCoroutine("NextWave");
+    }
+
+
+    IEnumerator NextWave()
+    {
+        yield return new WaitForSeconds(waveTime);
+        currentCreatures = GameObject.FindGameObjectsWithTag("Creature1");
+        
+        GameObject FarthestCreature;
+        GameObject SecondFarthestCreature;
+        float farthestDistance = 0;
+        float secondfarthestDistance = 0;
+        for (int i = 0; i < currentCreatures.Length; i++)
+        {
+            float currentDistance = currentCreatures[i].GetComponentInChildren<creatureScript>().distance;
+            if (currentDistance > farthestDistance)
+            {
+                secondfarthestDistance = farthestDistance;
+                farthestDistance = currentDistance;
+
+                SecondFarthestCreature = FarthestCreature;
+
+                FarthestCreature = currentCreatures[i];
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
